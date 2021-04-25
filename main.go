d0636e986af70d0ba7b8d4ccc1307f8506ac4d6f
Latiff, Mithun <Mithun.Latiff@sicpa.com>
2021-04-25 13:50:40 +0800

    Updated

--- main.go
+++ main.go
@@ -16,8 +16,9 @@ func main() {
 	dbPass := os.Getenv("DB_PASS")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
 
-	connection, err := driver.ConnectSQL(dbHost, dbPort, "root", dbPass, dbName)
+	connection, err := driver.ConnectSQL(dbHost, dbPort, dbUser, dbPass, dbName)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
